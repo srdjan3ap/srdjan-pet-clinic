425f4a9f0a6a8623674c0d0f06382edce6e84e73
Srdjan Obradovic <srdjan@3ap.ch>
2019-03-06 11:19:01 +0100

    no message

---
+++ src/main/java/com/srdjan/petclinic/SrdjanPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.srdjan.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrdjanPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrdjanPetClinicApplication.class, args);
+	}
+
+}
